75723d1e98b6a4e423a3cb8dff3add755ffe1c7b
Andrew Su <andrew.su@gmail.com>
2009-12-13 09:42:09 -0800

    update documentation of available services

--- genewikitools/PrintDoc.py
+++ genewikitools/PrintDoc.py
@@ -1,12 +1,27 @@
-print 'Content-Type: text/plain'
+print 'Content-Type: text/html'
 print ''
 
 print '''
+<pre>
 List of tools
 -------------
 
 /ListPages
-   print the page titles of all Gene Wiki pages
+   Input: None
+   Output: page titles of all Gene Wiki pages (JSON)
+   Example: <a href="/ListPages">/ListPages</a>
+   Note: returns the most recent entry in the data store
 
+/Pubmed2Mesh
+   Input: List of Pubmed IDs (comma separated)
+   Output: MeSH terms for each PMID (JSON)
+   Example: <a href="/Pubmed2Mesh?pmids=18464898,16492761">/Pubmed2Mesh?pmids=18464898,16492761</a>
 
+/RefreshPageList
+   Input: None
+   Output: confirmation message
+   Example: <a href="/RefreshPageList">/RefreshPageList</a>
+   Note: refreshes the Gene Wiki page list in the data store; typically called by cron only
+
+</pre>
 '''
