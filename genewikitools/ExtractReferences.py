f60164c133ab8df0a6cadf73fc3c31dcdde5ffe3
Stephanie Feudjio <Stephanie Feudjio@localhost>
2010-08-19 21:21:29 -0700

    Final version

--- genewikitools/ExtractReferences.py
+++ genewikitools/ExtractReferences.py
@@ -4,31 +4,32 @@ import sys
 from django.utils import simplejson as json
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
+import string
 
 class Reference ():
     def __init__(self):
-        self.sentence = ''
-        self.reference = ''
-        self.pmid = ''
-        self.mesh = []
-        self.geneId = ''
+	self.sentence = ''
+	self.reference = ''
+	self.pmid = ''
+	self.mesh = []
+	self.geneId = ''
 
     def parsePmid(self):
         matchObject = re.search("pmid[ =]*(\d+)",self.reference)
         if( not matchObject is None ):
-            self.pmid = matchObject.group(1)
+	    self.pmid = matchObject.group(1)
 
     def exportToHash(self):
-        output = {}
-        output['sentence'] = self.sentence
-        output['reference'] = self.reference
-        output['pmid'] = self.pmid
-        output['mesh'] = self.mesh
-        output['geneId'] = self.geneId
-        return(output)
+	output = {}
+	output['sentence'] = self.sentence
+	output['reference'] = self.reference
+	output['pmid'] = self.pmid
+	output['mesh'] = self.mesh
+	output['geneId'] = self.geneId
+	return(output)
 
 def getSentenceBefore(matchObject):
-    # Strategy:
+    # Strategy: 
     #   - retrieve document from beginning to start of match object
     #   - reverse that string
     #   - search for first occurrence of period or newline
@@ -40,64 +41,81 @@ def getSentenceBefore(matchObject):
 
     strBeforeReversed = strBefore[::-1]
     sentenceBreaks = re.finditer("[.\n]",strBeforeReversed)
-    sentence='';     st=[];   j=-1    ##- Used in case there is / are decimal in sentence
+    upp=''.join(string.ascii_letters)
+    title= [" Dr",' Pr',' Ms',' Mr',' al','Mrs','Miss','i.e','e.g',' C',' D' ]
+    N=''.join(string.punctuation)+''.join(string.digits)+upp
     for sentenceBreak in sentenceBreaks:
-        if sentenceBreak.start() < 5: ### skip if period occurs right before ref
-            continue
+	if sentenceBreak.start() < 5: ### skip if period occurs right before ref
+	    continue
         nextStop = sentenceBreak.start()
-        j=j+1;       st.append(nextStop) ##-
-        if re.match("^[0-9]$",strBefore[nextStop-1])and  re.match("^[0-9]$",strBefore[nextStop+1]): ##-
-            sentence= sentence+strBefore[-1*nextStop : -st[j]] ##-
-            
-            pass ##-
-        else:  ##-
-            sentencePreceding = strBefore[-1*nextStop:] ##-
-        #break
-        
-    sentencePreceding=sentence+sentencePreceding ##-
-    
+        T=strBeforeReversed[nextStop+1:nextStop+4]
+        T=T[::-1]
+##        spec=strBeforeReversed[nextStop+1:nextStop+3]
+##        print spec
+        if (T in title):
+            continue
+       # print "================= ", strBeforeReversed[nextStop-2], " ", strBeforeReversed[nextStop-2:nextStop+2]
+       #we take the 2 caracteres before and after the dot. If it is possible to convert the whole set into float then we go further
+        # skipping to the next stop
+        if ((strBeforeReversed[nextStop-1]in N) and (strBeforeReversed[nextStop+1] in N)):
+            continue
+        else:
+            pass
+            #print " No presence of word with an Extension"
+        try:  
+            float(strBeforeReversed[nextStop-1:nextStop+2])                                                                                                                                                                                                                                                                                             
+            continue
+        except:
+            pass
+            #print 'No Decimal Number here'      
+        if (len(strBefore[-1*nextStop:].split())<3):
+            continue
+	break
     # pull out sentence fragment, add a "^" to mark ref location
-    #sentencePreceding = strBefore[-1*nextStop:] Added up ##-
+    sentencePreceding = strBefore[-1*nextStop:]
     sentencePreceding = sentencePreceding + "^"
+
     ### if a period does not occur immediately before ref, then add after ref until
     ###    period or newline
 #    print "RE: ", re.search("\.\s*$",sentencePreceding )
     if( re.search("\.\s*\^$",sentencePreceding ) is None ):
-        strAfter = matchObject.string[matchObject.end():]
+	strAfter = matchObject.string[matchObject.end():]
         strAfter = re.sub("<ref[^>]*>[^<]*</ref>","",strAfter)  # remove all other refs
         strAfter = re.sub("<ref[^>]*>","",strAfter)  # remove <ref name=pmid8810341\/>
-        sentenceBreaks = re.finditer("[.\n]",strAfter)
-        st=[]; j=0; sentence=""; st.append(0) ##-
-        for sentenceBreak in sentenceBreaks:
-            nextStop = sentenceBreak.start()
-            j=j+1; st.append(nextStop) ##-
-            if( sentenceBreak.group(0) == "." ):
-                 stop=nextStop+1 #sentencePreceding = sentencePreceding + strAfter[0:nextStop+1]
-            else:
-                stop=nextStop #sentencePreceding = sentencePreceding + strAfter[0:nextStop]
-            if re.match("^[0-9]$",strAfter[nextStop-1])and  re.match("^[0-9]$",strAfter[nextStop+1]): ##-
-                sentence=sentence+strAfter[st[j-1]:stop+1] ##-
-                pass  ##-
-            else:  ##-
-                break  ##-
-        sentencePreceding = sentence+strAfter[st[j-1]+1:stop+1] ##-            
-   
+	sentenceBreaks = re.finditer("[.\n]",strAfter)
+	for sentenceBreak in sentenceBreaks:
+	    nextStop = sentenceBreak.start()
+	    #we take the 2 characters before and after the dot. If it is possible to convert the whole set into float then we go further
+	    #we skip this stop and go to the next Stop. Otherwise we continue with the instructions (the if statment...
+	    try:
+                float(strAfter[nextStop-1:nextStop+2])          
+                continue
+            except:
+                pass
+                #print ''
+	    if( sentenceBreak.group(0) == "." ):
+                sentencePreceding = sentencePreceding + strAfter[0:nextStop+1]
+	    else:
+                sentencePreceding = sentencePreceding + strAfter[0:nextStop]
+            #print strAfter[0:nextStop]
+	    break
+
     ### clean sentence of wiki-formatting characters
     # fix [[target|label]] type of wikilinks
     sentencePreceding = re.sub(r"\[\[[^]]*\|([^]|]*)\]\]",r"\1",sentencePreceding)
     # if line starts with a '|' character, label as a "Table" statement
     if( re.match("^(\|[+]*)",sentencePreceding )):
-        sentencePreceding = re.sub("^(\|[+]*)","",sentencePreceding)
+	sentencePreceding = re.sub("^(\|[+]*)","",sentencePreceding)
         sentencePreceding = "".join(["(Table)",sentencePreceding])
     # if line starts with a '#' or '*' character, label as a "List" statement
     if( re.match("^[#*]",sentencePreceding )):
-        sentencePreceding = re.sub("^([#*])","",sentencePreceding)
+	sentencePreceding = re.sub("^([#*])","",sentencePreceding)
         sentencePreceding = "".join(["(List)",sentencePreceding])
     # remove all other wikilinking syntax
     sentencePreceding = re.sub("['[\]]","",sentencePreceding)
     # remove newlines
     sentencePreceding = re.sub("\n","",sentencePreceding)
-    #print "xxxxxxxxx", sentencePreceding ##-           
+		
     return( sentencePreceding )
 
 ### Create a Reference object and populate it with info from MatchObject
@@ -109,7 +127,7 @@ def createReference( matchObject,entrezGeneId ):
     ref.parsePmid()
 #    print "MATCH: ",matchObject.start(),"-",matchObject.end()
 #    print "REF: ", ref.reference
-#    print "SENTENCE: ", ref.sentence #%%%%%%%%%%%%
+#    print "SENTENCE: ", ref.sentence
 #    print "PMID: ", ref.pmid
 #    print ''
     return(ref)
@@ -118,32 +136,32 @@ def createReference( matchObject,entrezGeneId ):
 def addMesh( references ):
     pmids = []
     for ref in references:
-        if( ref.pmid != ""):
-            pmids.append(ref.pmid)
+	if( ref.pmid != ""):
+	    pmids.append(ref.pmid)
 
     if( len(pmids) == 0 ):
-        return(references)
+	return(references)
 #    print "PMIDs: ", ",".join(pmids)
     url = "http://genewikitools.appspot.com/Pubmed2Mesh"
 #    url = "http://localhost:8081/Pubmed2Mesh"
     urlparams = {
          'pmids': ",".join(pmids)
-         }
+	 }
 #    try:
 #        f = urllib.urlopen( url, urllib.urlencode(urlparams) )
 #        z = f.read()
-#       print "Z: ", z
+#	print "Z: ", z
 #        meshIndex = json.loads(z)
 #    except:
-##      return(references)
-#       print 'Content-type: text/plain'
-#       print ''
-#       print "ExtractReferences: Error reading Pubmed2Mesh"
-#       print "URL: ", url
-#       print "urlparams: ", urlparams
-#       print "num pubmeds: ", len(pmids)
-#       print "Content: ", z
-#       sys.exit(1)
+##	return(references)
+#	print 'Content-type: text/plain'
+#	print ''
+#	print "ExtractReferences: Error reading Pubmed2Mesh"
+#	print "URL: ", url
+#	print "urlparams: ", urlparams
+#	print "num pubmeds: ", len(pmids)
+#	print "Content: ", z
+#	sys.exit(1)
 
 #    print "URL: ", url
 #    print "urlparams: ", urllib.urlencode(urlparams)
@@ -155,22 +173,22 @@ def addMesh( references ):
     try:
         meshIndex = json.loads(z)
     except:
-        print 'Content-type: text/plain'
-        print ''
-        print "ExtractReferences: Error reading Pubmed2Mesh"
-        print "URL: ", url
-        print "urlparams: ", urllib.urlencode(urlparams)
-        print "num pubmeds: ", len(pmids)
-        print "Content: ", z, "."
-        sys.exit(1)
+	print 'Content-type: text/plain'
+	print ''
+	print "ExtractReferences: Error reading Pubmed2Mesh"
+	print "URL: ", url
+	print "urlparams: ", urllib.urlencode(urlparams)
+	print "num pubmeds: ", len(pmids)
+	print "Content: ", z, "."
+	sys.exit(1)
 
 #    print z
 
     for ref in references:
-        if( ref.pmid != ""):
-#           print "PMID: ", ref.pmid
-#           print meshIndex[ref.pmid]
-            ref.mesh = meshIndex[ref.pmid]
+	if( ref.pmid != ""):
+#	    print "PMID: ", ref.pmid
+#	    print meshIndex[ref.pmid]
+	    ref.mesh = meshIndex[ref.pmid]
 
     return( references )
 
@@ -182,63 +200,62 @@ class ExtractReferences( webapp.RequestHandler ):
         return
 
     def get(self):
-#       print "Content-type: text/plain"
-#       print ''
-        self.setUrlRoot()
-        self.response.headers['Content-type'] = 'text/plain'
-#       print "SELF1: ", self.request.url
-        articleName = self.request.get('article')
-        articleName = re.sub(" ","_",articleName)
+#	print "Content-type: text/plain"
+#	print ''
+	self.setUrlRoot()
+	self.response.headers['Content-type'] = 'text/plain'
+#	print "SELF1: ", self.request.url
+	articleName = self.request.get('article')
+	articleName = re.sub(" ","_",articleName)
 
         content = self.getContent(articleName)
-        mo = re.search("{{PBB\|geneid=(\d+)}}",content)
-        entrezGeneId = mo.group(1)
-       
-        # match typical references like <ref>...</ref>
+	mo = re.search("{{PBB\|geneid=(\d+)}}",content)
+	entrezGeneId = mo.group(1)
+	
+	# match typical references like <ref>...</ref>
         refList = re.finditer( '(<ref[^<]*</ref>)', content )
-        references = []
-        for matchObject in refList:
+	references = []
+	for matchObject in refList:
             references.append( createReference(matchObject,entrezGeneId) )
 
-        # match other references like <ref name="..." />
+	# match other references like <ref name="..." />
         refList = re.finditer( '(<ref[^>]*/\s*>)', content )
-        for matchObject in refList:
+	for matchObject in refList:
             references.append( createReference(matchObject,entrezGeneId) )
 
-        references = addMesh( references )
-       
-        z = [ x.exportToHash() for x in references ]
-        self.response.out.write( json.dumps(z,indent=4) )
+	references = addMesh( references )
+
+	z = [ x.exportToHash() for x in references ]
+	self.response.out.write( json.dumps(z,indent=4) )
 
     def getContent(self, articleName):
-        url = ''.join(['http://',self.urlRoot,'/ReadGeneWikiPage?article=',articleName])
-        url = ''.join(['http://genewikitools.appspot.com/ReadGeneWikiPage?article=',articleName])
-
+	url = ''.join(['http://',self.urlRoot,'/ReadGeneWikiPage?article=',articleName])
+	url = ''.join(['http://genewikitools.appspot.com/ReadGeneWikiPage?article=',articleName])
 
 #        url = "http://localhost:8081/ReadGeneWikiPage?article=ITK_(gene)"
 #        url = "http://genewikitools.appspot.com/ReadGeneWikiPage?article=ITK_(gene)"
-#       url = "http://localhost:8081/ReadGeneWikiPage?article="+articleName
-#       print "URL:",url
-#       f = urllib.urlopen( "http://genewikitools.appspot.com/ReadGeneWikiPage?article=AKR1C1" )
-        try:
-            f = urllib.urlopen( url )
-            z = f.read()
-        except :
+#	url = "http://localhost:8081/ReadGeneWikiPage?article="+articleName
+#	print "URL:",url
+#	f = urllib.urlopen( "http://genewikitools.appspot.com/ReadGeneWikiPage?article=AKR1C1" )
+	try:
+	    f = urllib.urlopen( url )
+    	    z = f.read()
+	except :
             print 'Content-type: text/plain'
-            print ''
-            print "Something went wrong..."
-            print "URL: ", url
-            sys.exit(1)
+	    print ''
+	    print "Something went wrong..."
+	    print "URL: ", url
+	    sys.exit(1)
 
-        return z
+	return z
 
 ###
 ### MAIN
 ###
 
 application = webapp.WSGIApplication(
-                [('.*', ExtractReferences)],
-                debug=True)
+		[('.*', ExtractReferences)],
+		debug=True)
 
 def main():
     run_wsgi_app(application)
