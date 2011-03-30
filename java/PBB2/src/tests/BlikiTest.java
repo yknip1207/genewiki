6531a28fdd862e6bdcf2b1c9751b139c2f0583db
erikclarke <erikclarke@localhost>
2011-03-30 14:10:44 -0700

    messing around with xml parsing. test now finds appropriate cmcontinue attribute in returned xml tree and presents second list of wp articles using that attribute.

--- java/PBB2/src/tests/BlikiTest.java
+++ java/PBB2/src/tests/BlikiTest.java
@@ -6,18 +6,33 @@ package tests;
  * @author eclarke@gnf.org
  */
 
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.StringReader;
 import java.util.List;
+import java.io.File;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 
-import org.xml.sax.SAXException;
+import org.w3c.dom.CharacterData;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import org.xml.sax.InputSource;
+import org.xml.sax.helpers.XMLReaderFactory;
+
 import info.bliki.api.*;
 
 
 public class BlikiTest {
 
 	public static void testQueryCategoryMembers() {
+		
 		User user = new User("", "", "http://en.wikipedia.org/w/api.php");
 		user.login();
 		
@@ -29,7 +44,9 @@ public class BlikiTest {
 		}
 		
 		// if more results are available, use "cmcontinue" from the last query result
-		String[] valuePairs2 = {"list", "categorymembers", "cmtitle", "Category:Physics", "cmcontinue", "Awards|"};
+		String cmcontinue = findNodeInXml(rawXmlResponse, "categorymembers");
+		
+		String[] valuePairs2 = {"list", "categorymembers", "cmtitle", "Category:Physics", "cmcontinue", cmcontinue};
 		rawXmlResponse = connector.queryXML(user, valuePairs2);
 		if (rawXmlResponse == null) {
 			System.out.println("No XML result for query.");
@@ -37,6 +54,65 @@ public class BlikiTest {
 		System.out.println(rawXmlResponse);
 	}
 	
+	public static String findNodeInXml(String rawXmlResponse, String queryNode) {
+		
+//		File xmlfile = new File("C:\\tmp\\queryXMLoutput.xml");
+//		BufferedWriter writer = null;
+//		try {
+//			writer = new BufferedWriter(new FileWriter(xmlfile));
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//		
+//		try {
+//			writer.write(rawXmlResponse);
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//		
+//		try {
+//			writer.close();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+		
+		try {
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+			DocumentBuilder db = dbf.newDocumentBuilder();
+			InputSource input = new InputSource();
+			input.setCharacterStream(new StringReader(rawXmlResponse));
+			String cmcontinue = null;
+			Document doc = db.parse(input);
+			NodeList nodes = doc.getElementsByTagName(queryNode);
+			for (int i = 0; i < nodes.getLength(); i++) {
+				Element element = (Element) nodes.item(i);
+				// I was trying to make this more general for no particular reason,
+				// but I wound up just hardcoding some stuff into it for this application.
+				if(element.hasAttribute("cmcontinue")) {
+					cmcontinue = element.getAttribute("cmcontinue");
+				}
+			}
+			System.out.println("cmcontinue value: " + cmcontinue);
+			
+			return cmcontinue;
+		} catch (Exception e) {
+			System.out.println("Exception!");
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	
+
+	public static String getCharacterDataFromElement(Element e) {
+		Node child = e.getFirstChild();
+		if (child instanceof CharacterData) {
+			CharacterData cd = (CharacterData) child;
+			return cd.getData();
+		}
+		return null;
+	}
+
 	public static void main(String[] args) {
 		testQueryCategoryMembers();
 	}
