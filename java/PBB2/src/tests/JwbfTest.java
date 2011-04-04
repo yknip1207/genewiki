4063170400be27ff85e79bee2ece1e0aab675d7a
erikclarke <erikclarke@localhost>
2011-04-04 16:06:10 -0700

    example (commented out at the moment) of easy conditional write to wp page using jwbf. right now, it is a test of ProteinBox.setValuesFromSource method

--- java/PBB2/src/tests/JwbfTest.java
+++ java/PBB2/src/tests/JwbfTest.java
@@ -1,35 +1,40 @@
 package tests;
 
 import org.gnf.pbb.Authenticate;
-import org.gnf.wikiapi.User;
+import org.gnf.pbb.ProteinBox;
 
 import net.sourceforge.jwbf.core.contentRep.SimpleArticle;
 import net.sourceforge.jwbf.mediawiki.bots.*;
 
 
 public class JwbfTest {
+	public static final String TestPageTitle = "User:Pleiotrope/sandbox/test_gene_sandbox";
 
 	public static void main(String[] args) throws Exception{
 		MediaWikiBot bot = new MediaWikiBot("http://en.wikipedia.org/w/");
-		//User user = Authenticate.login("credentials.json", false);
-		if (bot.isLoggedIn() == false) {
-			System.out.print("Logging in... ");
-			bot.login("Pleiotrope", "Cynosura42");
-			System.out.println("Success!");
-		}
+		String[] credentials = Authenticate.login("credentials.json");
+		//XXX
+//		if (bot.isLoggedIn() == false) {
+//			System.out.print("Logging in... ");
+//			bot.login(credentials[0], credentials[1]);
+//			System.out.println("Success!");
+//		}
+//		
+//		SimpleArticle sa = new SimpleArticle(bot.readContent(TestPageTitle));
+//		String ArticleText = sa.getText();
+//		
+//		//SimpleArticle writeTest = new SimpleArticle();
+//		sa.setMinorEdit(true);
+//		if (!ArticleText.contains("Greetings from PBB2")) {
+//			//sa.addText("//n Another minor modification to the talk page by ~~~~");
+//			//bot.writeContent(sa);
+//		} else {
+//			System.out.println("Conditional modification failed; article was not modified.");
+//		}
 		
-		SimpleArticle sa = new SimpleArticle(bot.readContent("Talk:Arylsulfatase_A"));
-		String ArticleText = sa.getText();
-		System.out.println(ArticleText);
+		ProteinBox proteinBox = new ProteinBox("410");
+		proteinBox.setValuesFromSource(410);
 		
-		//SimpleArticle writeTest = new SimpleArticle();
-		sa.setMinorEdit(true);
-		if (!ArticleText.contains("Greetings from PBB2")) {
-			//sa.addText("//n Another minor modification to the talk page by ~~~~");
-			//bot.writeContent(sa);
-		} else {
-			System.out.println("Conditional modification failed; article was not modified.");
-		}
 		
 	}
 	
