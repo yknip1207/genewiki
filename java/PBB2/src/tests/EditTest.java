2900773ce46bb7dd59878ab344907bda286d5d20
erikclarke <erikclarke@localhost>
2011-04-04 16:05:05 -0700

    most of the methods in this class use bliki; now using jwbf framework for bot so this is outdated and will prob be deleted

--- java/PBB2/src/tests/EditTest.java
+++ java/PBB2/src/tests/EditTest.java
@@ -4,6 +4,7 @@ import org.gnf.pbb.Authenticate;
 import org.gnf.pbb.editor.XmlParser;
 import org.gnf.wikiapi.Connector;
 import org.gnf.wikiapi.User;
+import org.gnf.wikiapi.query.Edit;
 import org.gnf.wikiapi.query.RequestBuilder;
 
 
@@ -18,8 +19,9 @@ public class EditTest {
 		// Logging in to wikipedia...
 		User user = null;
 		try {
-			user = Authenticate.login("credentials.json");
-			System.out.println(user.getToken());
+			//user = Authenticate.login("credentials.json");
+			//System.out.println(user.getToken());
+			throw new Exception ("This class is deprecated, do not run.");
 		} catch (Exception e) {
 			System.out.println("There was an error authenticating.");
 			e.printStackTrace();
@@ -29,16 +31,19 @@ public class EditTest {
 		
 		// Find the specified page and make a minor edit
 		Connector connector = new Connector();
-		setEditToken(user, connector);
-		RequestBuilder rb = new RequestBuilder();
-		rb.action("edit");
-		rb.format("json");
-		rb.put("title", "Talk:"+testGeneTitle);
-		rb.put("section", "new");
-		rb.put("summary", "Hello World!");
-		rb.put("text", "Greetings from PBB2!");
-		rb.put("token", "+\\");
-		System.out.println(rb.toString());
+		try{
+			setEditToken(user, connector);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		Edit edit = Edit.create();
+		//rb.format("json");
+		edit.title("Talk:"+testGeneTitle);
+		edit.section("new"); //TODO: check if section is already created
+		edit.summary("Hello World!");
+		edit.text("Greetings from PBB2!");
+		edit.token(token); //TODO: do we have a valid token?
+		System.out.println(edit.toString());
 		// Actually write something:
 		//System.out.println(connector.sendXML(user, rb));
 		
@@ -46,8 +51,8 @@ public class EditTest {
 		
 	}
 
-	private static void setEditToken(User user, Connector connector) {
-		String[] valuePairs = {"post", "info|revisions", "intoken", "edit", "titles", testGeneTitle};
+	private static void setEditToken(User user, Connector connector) throws Exception {
+		String[] valuePairs = {"prop", "info", "intoken", "edit", "titles", "Main_Page"};
 		String rawXmlResponse = connector.queryXML(user, valuePairs);
 		System.out.println(rawXmlResponse); //XXX
 		if (rawXmlResponse == null) {
@@ -56,6 +61,9 @@ public class EditTest {
 			token = XmlParser.findAttributeInXml(rawXmlResponse, "page", "edittoken");
 			if (token == null) {
 				System.out.println("Edit token returned null; unable to continue.");
+				throw new Exception("Failed to get non-null edit token.");
+			} else if (token == "+\\") {
+				throw new Exception("Edit token represents anonymous user... something went wrong.");
 			}
 		}
 	}
