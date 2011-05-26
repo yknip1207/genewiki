373b505520f882a6fa16c8582a0e8501ff73cba4
erikclarke <erikclarke@localhost>
2011-05-25 19:06:13 -0700

    Added a sleep to the wait loop and a method to automatically find templates to update (kind of)

--- java/PBB2/src/org/gnf/pbb/Launcher.java
+++ java/PBB2/src/org/gnf/pbb/Launcher.java
@@ -1,14 +1,20 @@
 package org.gnf.pbb;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.logging.Logger;
 
 import org.gnf.pbb.controller.PBBController;
 import org.gnf.pbb.exceptions.PbbExceptionHandler;
 import org.gnf.pbb.exceptions.Severity;
+import org.gnf.pbb.exceptions.ValidationException;
+import org.gnf.pbb.logs.DatabaseManager;
+import org.gnf.pbb.util.Find;
 
 
 /**
@@ -22,8 +28,20 @@ public class Launcher {
 		Logger logger = Logger.getLogger(Launcher.class.getName());
 		Configs.GET.setConfigsFromFile("BotConfigs.json");
 		PbbExceptionHandler exHandler = PbbExceptionHandler.INSTANCE;
-		
+		DatabaseManager dbManager = new DatabaseManager();
+		try {
+			dbManager.init();
+		} catch (ValidationException e1) {}
 		List<String> inputs = new ArrayList<String>(0);
+		
+		if (args.length < 1) {
+			System.out.println("Retrieving some gene ids to play with instead.");
+			inputs  = Find.updateCandidates(3, Configs.GET);
+			Collections.shuffle(inputs);
+			System.out.println(inputs);
+			System.out.println("Press any key to continue.");
+			System.in.read();
+		}
 		
 		for (String geneId : args) {
 			try {
@@ -34,18 +52,19 @@ public class Launcher {
 			}
 		}
 		
+		// We're running the bot controller in a thread to allow us to monitor the bot's state 
 		Thread controller = new Thread(new PBBController(inputs));
 		System.out.println("Starting bot controller now with supplied arguments. \n" +
 				"Press q+enter to end bot operation and see completion report.");
 		controller.start();
 
 		while (controller.isAlive()) {
-//			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//			if (br.readLine().equalsIgnoreCase("q")) {
-//				controller.interrupt();
-//				controller.join();
-//				logger.info("Keyboard interrupt detected.");
-//			}
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			if (br.readLine().equalsIgnoreCase("q")) {
+				controller.interrupt();
+				controller.join();
+				logger.info("Keyboard interrupt detected.");
+			}
 			if (exHandler.checkState().compareTo(Severity.FATAL) > 0) {
 				logger.severe("Bot failure.");
 				logger.severe(exHandler.printExceptionStackTraces(exHandler.getExceptionsOfSeverity(Severity.FATAL)));
@@ -54,7 +73,7 @@ public class Launcher {
 				controller.join(10000);
 				
 			}
-			controller.join();
+			Thread.sleep(500);
 		}
 	}
 }
